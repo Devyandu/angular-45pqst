20359a50299fd6bedc606ee8072c443c05e20f3f
Devyandu <devyandu@gmail.com>
2020-12-13 11:17:20 -0500

    2 files modified, 1 added

--- src/app/benefits-list/benefits-list.component.ts
+++ src/app/benefits-list/benefits-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Benefit } from "../benefit";
 
 @Component({
-  selector: 'app-benefits-list',
-  templateUrl: './benefits-list.component.html',
-  styleUrls: ['./benefits-list.component.css']
+  selector: "app-benefits-list",
+  templateUrl: "./benefits-list.component.html",
+  styleUrls: ["./benefits-list.component.css"]
 })
 export class BenefitsListComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  benefits: Benefit[];
 
   ngOnInit() {
+    this.benefits = [
+      {
+        icon: "doctor",
+        name: "ovcopay",
+        title: "Office Visit Co-Pay",
+        value: "$25 per visit"
+      },
+      {
+        icon: "doctor",
+        name: "ovcopay",
+        title: "Office Visit Co-Pay",
+        value: "$25 per visit"
+      },
+      {
+        icon: "doctor",
+        name: "ovcopay",
+        title: "Office Visit Co-Pay",
+        value: "$25 per visit"
+      },
+      {
+        icon: "doctor",
+        name: "ovcopay",
+        title: "Office Visit Co-Pay",
+        value: "$25 per visit"
+      }
+    ];
   }
-
 }
